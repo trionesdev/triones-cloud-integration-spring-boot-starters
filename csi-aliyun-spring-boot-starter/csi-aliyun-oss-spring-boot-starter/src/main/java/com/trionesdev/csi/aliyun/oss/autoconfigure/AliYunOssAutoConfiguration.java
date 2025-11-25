b18fef71246197fa926b1ebdd8ee5392aac49ac3
落雁沙 <fengxiaotx@163.com>
2025-11-25 18:55:17 +0800

    feat: update folder

--- csi-aliyun-spring-boot-starter/csi-aliyun-oss-spring-boot-starter/src/main/java/com/trionesdev/csi/aliyun/oss/autoconfigure/AliYunOssAutoConfiguration.java
+++ csi-aliyun-spring-boot-starter/csi-aliyun-oss-spring-boot-starter/src/main/java/com/trionesdev/csi/aliyun/oss/autoconfigure/AliYunOssAutoConfiguration.java
@@ -3,19 +3,10 @@ package com.trionesdev.csi.aliyun.oss.autoconfigure;
 import com.trionesdev.csi.aliyun.oss.AliYunOSS;
 import com.trionesdev.csi.aliyun.oss.AliYunOssConfig;
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.ConstructorArgumentValues;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import static com.trionesdev.csi.aliyun.oss.autoconfigure.AliYunOssProperties.PREFIX;
 
@@ -25,36 +16,48 @@ import static com.trionesdev.csi.aliyun.oss.autoconfigure.AliYunOssProperties.PR
 @EnableConfigurationProperties(value = {
         AliYunOssProperties.class
 })
-public class AliYunOssAutoConfiguration implements EnvironmentAware, BeanFactoryPostProcessor {
+public class AliYunOssAutoConfiguration {
     private AliYunOssProperties ossProperties;
 
-    @Override
-    public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
-        AliYunOssConfig aliYunOssProperties = AliYunOssConfig.builder()
+//    @Override
+//    public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+//        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+//        AliYunOssConfig aliYunOssProperties = AliYunOssConfig.builder()
+//                .accessKeyId(ossProperties.getAccessKeyId())
+//                .accessKeySecret(ossProperties.getAccessKeySecret())
+//                .endpoint(ossProperties.getEndpoint())
+//                .bucket(ossProperties.getBucket())
+//                .urlPrefix(ossProperties.getUrlPrefix())
+//                .build();
+//        ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+//        argumentValues.addIndexedArgumentValue(0, aliYunOssProperties);
+//        registerBean(beanFactory, argumentValues, AliYunOSS.class.getName());
+//
+//    }
+//
+//    @Override
+//    public void setEnvironment(@NotNull Environment environment) {
+//        this.ossProperties = Binder.get(environment).bind(PREFIX, AliYunOssProperties.class).get();
+//    }
+//
+//    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
+//        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+//        beanDefinition.setBeanClass(AliYunOSS.class);
+//        beanDefinition.setBeanClassName(AliYunOSS.class.getName());
+//        beanDefinition.setConstructorArgumentValues(argumentValues);
+//        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+//    }
+
+    @Bean
+    public AliYunOSS aliYunOSS() throws Exception {
+        AliYunOssConfig aliYunOssConfig = AliYunOssConfig.builder()
                 .accessKeyId(ossProperties.getAccessKeyId())
                 .accessKeySecret(ossProperties.getAccessKeySecret())
                 .endpoint(ossProperties.getEndpoint())
                 .bucket(ossProperties.getBucket())
                 .urlPrefix(ossProperties.getUrlPrefix())
                 .build();
-        ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-        argumentValues.addIndexedArgumentValue(0, aliYunOssProperties);
-        registerBean(beanFactory, argumentValues, AliYunOSS.class.getName());
-
-    }
-
-    @Override
-    public void setEnvironment(@NotNull Environment environment) {
-        this.ossProperties = Binder.get(environment).bind(PREFIX, AliYunOssProperties.class).get();
-    }
-
-    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
-        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-        beanDefinition.setBeanClass(AliYunOSS.class);
-        beanDefinition.setBeanClassName(AliYunOSS.class.getName());
-        beanDefinition.setConstructorArgumentValues(argumentValues);
-        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        return new AliYunOSS(aliYunOssConfig);
     }
 
 }
--- csi-aliyun-spring-boot-starter/csi-aliyun-sms-spring-boot-starter/src/main/java/com/trionesdev/csi/aliyun/sms/autoconfigure/AliYunSmsAutoConfiguration.java
+++ csi-aliyun-spring-boot-starter/csi-aliyun-sms-spring-boot-starter/src/main/java/com/trionesdev/csi/aliyun/sms/autoconfigure/AliYunSmsAutoConfiguration.java
@@ -3,20 +3,10 @@ package com.trionesdev.csi.aliyun.sms.autoconfigure;
 import com.trionesdev.csi.aliyun.sms.AliYunSms;
 import com.trionesdev.csi.aliyun.sms.AliYunSmsConfig;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.ConstructorArgumentValues;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import static com.trionesdev.csi.aliyun.sms.autoconfigure.AliYunSmsProperties.PREFIX;
 
@@ -24,13 +14,41 @@ import static com.trionesdev.csi.aliyun.sms.autoconfigure.AliYunSmsProperties.PR
 @Configuration
 @ConditionalOnProperty(prefix = PREFIX, value = {"enabled"}, havingValue = "true")
 @EnableConfigurationProperties(value = {AliYunSmsProperties.class})
-public class AliYunSmsAutoConfiguration implements EnvironmentAware, BeanFactoryPostProcessor {
+public class AliYunSmsAutoConfiguration   {
     private AliYunSmsProperties aliYunSmsProperties;
 
-    @SneakyThrows
-    @Override
-    public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+//    @SneakyThrows
+//    @Override
+//    public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+//        DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) configurableListableBeanFactory;
+//        AliYunSmsConfig aliYunSmsConfig = AliYunSmsConfig.builder()
+//                .accessKeyId(aliYunSmsProperties.getAccessKeyId())
+//                .accessKeySecret(aliYunSmsProperties.getAccessKeySecret())
+//                .regionId(aliYunSmsProperties.getRegionId())
+//                .signName(aliYunSmsProperties.getSignName())
+//                .templateCodes(aliYunSmsProperties.getTemplateCodes())
+//                .build();
+//        ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
+//        argumentValues.addIndexedArgumentValue(0, aliYunSmsConfig);
+//        registerBean(beanFactory, argumentValues, AliYunSms.class.getName());
+//
+//    }
+//
+//    @Override
+//    public void setEnvironment(@NotNull Environment environment) {
+//        this.aliYunSmsProperties = Binder.get(environment).bind(PREFIX, AliYunSmsProperties.class).get();
+//    }
+//
+//    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
+//        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+//        beanDefinition.setBeanClass(AliYunSms.class);
+//        beanDefinition.setBeanClassName(AliYunSms.class.getName());
+//        beanDefinition.setConstructorArgumentValues(argumentValues);
+//        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+//    }
+
+    @Bean
+    public AliYunSms aliYunSms() throws Exception {
         AliYunSmsConfig aliYunSmsConfig = AliYunSmsConfig.builder()
                 .accessKeyId(aliYunSmsProperties.getAccessKeyId())
                 .accessKeySecret(aliYunSmsProperties.getAccessKeySecret())
@@ -38,23 +56,7 @@ public class AliYunSmsAutoConfiguration implements EnvironmentAware, BeanFactory
                 .signName(aliYunSmsProperties.getSignName())
                 .templateCodes(aliYunSmsProperties.getTemplateCodes())
                 .build();
-        ConstructorArgumentValues argumentValues = new ConstructorArgumentValues();
-        argumentValues.addIndexedArgumentValue(0, aliYunSmsConfig);
-        registerBean(beanFactory, argumentValues, AliYunSms.class.getName());
-
-    }
-
-    @Override
-    public void setEnvironment(@NotNull Environment environment) {
-        this.aliYunSmsProperties = Binder.get(environment).bind(PREFIX, AliYunSmsProperties.class).get();
-    }
-
-    private void registerBean(DefaultListableBeanFactory beanFactory, ConstructorArgumentValues argumentValues, String beanName) {
-        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
-        beanDefinition.setBeanClass(AliYunSms.class);
-        beanDefinition.setBeanClassName(AliYunSms.class.getName());
-        beanDefinition.setConstructorArgumentValues(argumentValues);
-        beanFactory.registerBeanDefinition(beanName, beanDefinition);
+        return new AliYunSms(aliYunSmsConfig);
     }
 
 }
